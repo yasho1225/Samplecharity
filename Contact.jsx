a497b3c6ef0b6e1fc27c976abcb11ac18de49612
yasho1225 <yashoin1225@gmail.com>
2025-05-31 20:26:52 -0400

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div className="min-h-screen bg-white text-black pt-20 font-poppins">
+      <div className="container mx-auto px-6 text-center">
+        <h1 className="text-4xl font-bold mb-4">Contact</h1>
+        <p>Placeholder for Contact page content.</p>
+      </div>
+    </div>
+  );
+}
