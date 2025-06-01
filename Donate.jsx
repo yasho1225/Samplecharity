66a74f74d212efa8384608a574f84bef9a1c887a
yasho1225 <yashoin1225@gmail.com>
2025-05-31 20:27:25 -0400

    Create Donate.jsx

---
+++ Donate.jsx
@@ -0,0 +1,10 @@
+function Donate() {
+  return (
+    <div className="min-h-screen bg-white text-black pt-20 font-poppins">
+      <div className="container mx-auto px-6 text-center">
+        <h1 className="text-4xl font-bold mb-4">Donate</h1>
+        <p>Placeholder for Donate page content.</p>
+      </div>
+    </div>
+  );
+}
