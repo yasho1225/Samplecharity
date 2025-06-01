687a4712f72f36733a52828e40404a898a6febf1
yasho1225 <yashoin1225@gmail.com>
2025-05-31 20:26:22 -0400

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div className="min-h-screen bg-white text-black pt-20 font-poppins">
+      <div className="container mx-auto px-6 text-center">
+        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <p>Placeholder for About Us page content.</p>
+      </div>
+    </div>
+  );
+}
